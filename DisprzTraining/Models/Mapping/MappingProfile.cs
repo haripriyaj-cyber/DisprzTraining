1dc2955accdb0f3301403a4a5768b43e922b33a9
haripriyaj-cyber <haripriya.j@disprz.com>
2025-09-24 12:22:38 +0530

    final bacend unit testing

--- DisprzTraining/Models/Mapping/MappingProfile.cs
+++ DisprzTraining/Models/Mapping/MappingProfile.cs
@@ -9,24 +9,32 @@ namespace DisprzTraining.Utils
     {
         public MappingProfile()
         {
-            // Appointment -> AppointmentDTO (this mapping is fine)
+            // Appointment -> AppointmentDTO
             CreateMap<Appointment, AppointmentDTO>();
             
             // CreateAppointmentDTO -> Appointment
             CreateMap<CreateAppointmentDTO, Appointment>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
-                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore());
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore()); // Ignore User navigation property
             
             // UpdateAppointmentDTO -> Appointment
             CreateMap<UpdateAppointmentDTO, Appointment>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
-                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(dest => dest.UserId, opt => opt.Ignore()) // Ignore UserId
+                .ForMember(dest => dest.User, opt => opt.Ignore()); // Ignore User navigation property
             
-            // Add these mappings
+            // User -> UserDTO
             CreateMap<User, UserDTO>();
-            CreateMap<RegisterDTO, User>();
+            
+            // RegisterDTO -> User
+            CreateMap<RegisterDTO, User>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore()) // Ignore Id
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow)) // Set CreatedAt
+                .ForMember(dest => dest.Appointments, opt => opt.Ignore()); // Ignore Appointments collection
         }
     }
 }
