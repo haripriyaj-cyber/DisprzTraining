48f5726b3e6f48c8090f8bcf879eb59a5410d02b
haripriyaj-cyber <haripriya.j@disprz.com>
2025-09-25 15:23:59 +0530

    added report generation

--- DisprzTraining.Tests/obj/Debug/net9.0/DisprzTraining.Tests.AssemblyInfo.cs
+++ DisprzTraining.Tests/obj/Debug/net9.0/DisprzTraining.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DisprzTraining.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eacb09bc850cd9dd73045838157ce756b04eb800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+413806afbbbcbdd6d85aeb18b0bbdee972ba3bec")]
 [assembly: System.Reflection.AssemblyProductAttribute("DisprzTraining.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DisprzTraining.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DisprzTraining/obj/Debug/net9.0/DisprzTraining.AssemblyInfo.cs
+++ DisprzTraining/obj/Debug/net9.0/DisprzTraining.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DisprzTraining")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eacb09bc850cd9dd73045838157ce756b04eb800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+413806afbbbcbdd6d85aeb18b0bbdee972ba3bec")]
 [assembly: System.Reflection.AssemblyProductAttribute("DisprzTraining")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DisprzTraining")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
